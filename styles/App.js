00f013613f390f073504dcb4767f2a801beec79a
kunstaytrue <st10487104@rcconnect.edu.za>
2025-11-10 17:48:46 +0200

    .

--- styles/App.js
+++ styles/App.js
@@ -1,5 +1,11 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { MenuProvider } from "./context/MenuContext";
+
+import HomeScreen from "./screens/HomeScreen";
+import ManageScreen from "./screens/ManageScreen";
+import FilterScreen from "./screens/FilterScreen";
 
 const Stack = createStackNavigator();
 
@@ -16,22 +22,3 @@ export default function App() {
     </MenuProvider>
   );
 }
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
-const Stack = createStackNavigator();
-
-export default function App() {
-  return (
-    <MenuProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Manage" component={ManageScreen} />
-          <Stack.Screen name="Filter" component={FilterScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </MenuProvider>
-  );
-}
-
